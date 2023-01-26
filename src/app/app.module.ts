a601296d6a31dccfa83a85dcee701304c64d8fdc
Marcel <m.meyer@it-frankfurt.com>
2023-01-26 15:14:22 +0100

    added task-list component and stories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './components/task.component';
+import { TaskListComponent } from './components/task-list.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,
+  TaskComponent, TaskListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
